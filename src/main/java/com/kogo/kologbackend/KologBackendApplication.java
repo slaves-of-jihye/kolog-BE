1a697c2606a1b5272e811c06dd9eb58141090c91
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-04 00:54:34 +0900

    feat: 프로젝트 초기 환경 설정 완료

---
+++ src/main/java/com/kogo/kologbackend/KologBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kogo.kologbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KologBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KologBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kogo/kologbackend/KologBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kogo.kologbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KologBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
