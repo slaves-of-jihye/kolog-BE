34483c53767c83265fd5ad7a9e72608e2e6457b4
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-05 01:00:23 +0900

    feat: UserController 및 핸들러 생성

---
+++ src/main/java/com/kogo/kologbackend/adapter/user/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.kogo.kologbackend.adapter.user.controller;
+
+import com.kogo.kologbackend.adapter.auth.dto.response.ApiResponse;
+import com.kogo.kologbackend.adapter.auth.provider.JwtProvider;
+import com.kogo.kologbackend.application.user.dto.response.UserProfileResponse;
+import com.kogo.kologbackend.application.user.internal.UserProfileUpdateUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserProfileUpdateUseCase userProfileUpdateUseCase;
+    private final JwtProvider jwtProvider;
+
+    @PatchMapping("/profile")
+    public ResponseEntity<ApiResponse<UserProfileResponse>> updateProfile(
+            @RequestHeader("Authorization") String token,
+            @RequestParam("nickname") String nickname,
+            @RequestParam(value = "profileImage", required = false) MultipartFile profileImage
+            ) {
+        String jwt = token.substring(7);
+        Long userId = jwtProvider.getUserIdFromToken(jwt);
+        UserProfileResponse data = userProfileUpdateUseCase.updateProfile(userId, nickname, profileImage);
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                200,
+                "프로필 설정 성공",
+                data
+        ));
+    }
+}
