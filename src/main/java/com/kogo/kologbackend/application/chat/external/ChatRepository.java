e4e9a4c6601655443c81d57696187dc3c1aa92ec
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-08 22:51:36 +0900

    feat: ChatRepository에 로그 id로 조회하는 메서드 생성

--- src/main/java/com/kogo/kologbackend/application/chat/external/ChatRepository.java
+++ src/main/java/com/kogo/kologbackend/application/chat/external/ChatRepository.java
@@ -3,6 +3,8 @@ package com.kogo.kologbackend.application.chat.external;
 import com.kogo.kologbackend.domain.chat.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRepository extends JpaRepository<Chat, Long> {
+import java.util.List;
 
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+    List<Chat> findByLogId(Long logId);
 }
