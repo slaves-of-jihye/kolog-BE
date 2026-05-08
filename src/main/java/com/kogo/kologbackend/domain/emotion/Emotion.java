84ba109e46dd3ea37a4bd33ef0daa82a90d117a5
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-08 22:11:57 +0900

    feat: emotion 엔티티 생성

---
+++ src/main/java/com/kogo/kologbackend/domain/emotion/Emotion.java
@@ -0,0 +1,38 @@
+package com.kogo.kologbackend.domain.emotion;
+
+import com.kogo.kologbackend.domain.log.Log;
+import com.kogo.kologbackend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "emotions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Emotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private int emotionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "log_id")
+    private Log log;
+
+    @Builder
+    public Emotion(int emotionId, User user, Log log) {
+        this.emotionId = emotionId;
+        this.user = user;
+        this.log = log;
+    }
+}
