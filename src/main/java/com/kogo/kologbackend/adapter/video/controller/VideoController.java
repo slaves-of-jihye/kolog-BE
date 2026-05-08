1e83fc8c1b8be0a6bf606199025ddf4d39bac853
정현태 <jht3021@gmail.com>
2026-05-08 23:38:32 +0900

    feat : 비디오 컨트롤러 이모티콘 추가, 댓글 추가 개발

---
+++ src/main/java/com/kogo/kologbackend/adapter/video/controller/VideoController.java
@@ -0,0 +1,56 @@
+package com.kogo.kologbackend.adapter.video.controller;
+
+import com.kogo.kologbackend.adapter.auth.dto.response.ApiResponse;
+import com.kogo.kologbackend.adapter.auth.provider.JwtProvider;
+import com.kogo.kologbackend.application.chat.dto.request.ChatCreateRequest;
+import com.kogo.kologbackend.application.chat.internal.ChatCreateUseCase;
+import com.kogo.kologbackend.application.emotion.dto.request.EmotionCreateRequest;
+import com.kogo.kologbackend.application.emotion.internal.EmotionCreateUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/video")
+public class VideoController {
+
+    private final EmotionCreateUseCase emotionCreateUseCase;
+    private final ChatCreateUseCase chatCreateUseCase;
+    private final JwtProvider jwtProvider;
+
+
+    @PostMapping("/emotion")
+    public ResponseEntity<ApiResponse> emotionCreate(
+            //@RequestHeader("Authorization") String token,
+            @RequestBody EmotionCreateRequest request
+    ) {
+        /*String jwt = token.substring(7);
+        Long userId = jwtProvider.getUserIdFromToken(jwt);
+
+        if (userId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }*/
+
+        emotionCreateUseCase.createEmotion(request);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, "이모티콘 표시 성공", request.emotionId()));
+    }
+
+    @PostMapping("/chat")
+    public ResponseEntity<ApiResponse> chatCreate(
+           // @RequestHeader("Authorization") String token,
+            @RequestBody ChatCreateRequest request
+            ){
+        //String jwt = token.substring(7);
+        //Long userId = jwtProvider.getUserIdFromToken(jwt);
+
+        //if (userId == null) {
+        //    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        //}
+
+        chatCreateUseCase.createChat(request);
+        return ResponseEntity.ok(new ApiResponse<>(200,"댓글 작성 성공",request.chatContent()));
+    }
+}
