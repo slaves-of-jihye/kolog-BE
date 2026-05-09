ebd24b6cd9ef0450e85b8bed91ab0450e47c8181
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-09 22:50:05 +0900

    refactor: 구글 로그인 관련 로직/의존성 삭제 및 스프링 시큐리티 의존성 추가

--- src/main/java/com/kogo/kologbackend/adapter/auth/dto/request/GoogleLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.kogo.kologbackend.adapter.auth.dto.request;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record GoogleLoginRequest(
-        String code,
-        @JsonProperty("redirect_uri")
-        String redirectUri
-) {}
--- src/main/java/com/kogo/kologbackend/adapter/auth/dto/response/AuthResponse.java
+++ src/main/java/com/kogo/kologbackend/adapter/auth/dto/response/AuthResponse.java
@@ -3,7 +3,6 @@ package com.kogo.kologbackend.adapter.auth.dto.response;
 public record AuthResponse(
         String accessToken,
         String refreshToken,
-        boolean isNewUser,
         UserDto user
 ) {
     public record UserDto(Long id, String email) {}
