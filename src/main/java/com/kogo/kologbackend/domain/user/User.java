7c619daeaa3bb53e833c117535ac4865a94dbea0
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-09 22:57:54 +0900

    feat: User 엔티티 내부에 비밀번호 필드 생성

--- src/main/java/com/kogo/kologbackend/domain/user/User.java
+++ src/main/java/com/kogo/kologbackend/domain/user/User.java
@@ -17,6 +17,9 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
     @Embedded
     @Setter
     private UserInfo userInfo = new UserInfo();
