ccd4a6dbfe20bfb1d9cc5987eeafbc94b0aa2eae
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-09 23:33:38 +0900

    feat: 로그인 핸들러 생성

--- src/main/java/com/kogo/kologbackend/adapter/auth/controller/AuthController.java
+++ src/main/java/com/kogo/kologbackend/adapter/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.kogo.kologbackend.adapter.auth.controller;
 
 import com.kogo.kologbackend.adapter.auth.Service.AuthService;
+import com.kogo.kologbackend.adapter.auth.dto.request.LoginRequest;
 import com.kogo.kologbackend.adapter.auth.dto.request.SignupRequest;
 import com.kogo.kologbackend.adapter.auth.dto.response.ApiResponse;
 import com.kogo.kologbackend.adapter.auth.dto.response.AuthResponse;
+import com.kogo.kologbackend.adapter.auth.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +26,18 @@ public class AuthController {
         return new ApiResponse<>(
                 201,
                 "회원가입 완료",
+                data
+        );
+    }
+
+    @PostMapping
+    public ApiResponse<LoginResponse> login(
+            @RequestBody LoginRequest loginRequest
+            ) {
+        LoginResponse data = authService.login(loginRequest);
+        return new ApiResponse<>(
+                200,
+                "로그인 성공",
                 data
         );
     }
