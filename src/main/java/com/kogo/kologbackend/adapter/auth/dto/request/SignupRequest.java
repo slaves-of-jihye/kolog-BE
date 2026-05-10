e4ff8630447b7b34db5f00c89d291d94e4f447eb
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-10 21:51:41 +0900

    refactor: 회원가입 requestDto에 닉네임 필드 추가

--- src/main/java/com/kogo/kologbackend/adapter/auth/dto/request/SignupRequest.java
+++ src/main/java/com/kogo/kologbackend/adapter/auth/dto/request/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.kogo.kologbackend.adapter.auth.dto.request;
 
 public record SignupRequest(
-        String email, String password
+        String email,
+        String password,
+        String nickname
 ) {}
