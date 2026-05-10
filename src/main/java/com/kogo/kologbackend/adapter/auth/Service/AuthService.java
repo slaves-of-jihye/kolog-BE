4e4f81712b9e85781ff4888a4b4dc2469baf54a5
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-05-10 21:57:53 +0900

    feat: 닉네임 중복 방지 기능 추가

--- src/main/java/com/kogo/kologbackend/adapter/auth/Service/AuthService.java
+++ src/main/java/com/kogo/kologbackend/adapter/auth/Service/AuthService.java
@@ -27,7 +27,9 @@ public class AuthService {
             throw new RuntimeException("이미 존재하는 이메일입니다.");
         }
 
-
+        if (userRepository.existsByNickname(signupRequest.nickname())) {
+            throw new RuntimeException("이미 존재하는 닉네임입니다.");
+        }
 
         User user = User.builder()
                 .email(signupRequest.email())
