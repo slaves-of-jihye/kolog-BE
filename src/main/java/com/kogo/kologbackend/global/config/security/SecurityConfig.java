1454186e066fa5bd1933487e11be0b9035aa0ae4
정현태 <jht3021@gmail.com>
2026-05-13 06:37:53 +0900

    chore : cors 오류 해결 시도

--- src/main/java/com/kogo/kologbackend/global/config/security/SecurityConfig.java
+++ src/main/java/com/kogo/kologbackend/global/config/security/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +26,26 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
 
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 Origin
+        configuration.addAllowedMethod("*");                    // 모든 HTTP Method 허용
+        configuration.addAllowedHeader("*");                    // 모든 Header 허용
+        configuration.setAllowCredentials(true);               // 쿠키/인증정보 허용 시 필수
+        configuration.setMaxAge(3600L);                         // Preflight 캐싱 시간
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 적용
+        return source;
     }
 }
