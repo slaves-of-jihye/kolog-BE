5384efbe32f5f5ec55838cd59a62fb65a444f57b
정현태 <jht3021@gmail.com>
2026-05-12 09:29:43 +0900

    cors 설정

---
+++ src/main/java/com/kogo/kologbackend/global/config/WebCorsConfig.java
@@ -0,0 +1,18 @@
+package com.kogo.kologbackend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
