d75f97691485e8a028acbad302d061ddd413d1b7
정현태 <jht3021@gmail.com>
2026-07-24 23:35:26 +0900

    오타 수정

--- src/main/java/com/kogo/kologbackend/adapter/user/controller/UserController.java
+++ src/main/java/com/kogo/kologbackend/adapter/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestControllerrm -f .git/index.lock
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
